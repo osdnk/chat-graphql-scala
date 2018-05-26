6261b31dc4580006ebc28cdb9523c098ef37eb05
osdnk <micosa97@gmail.com>
2018-05-26 21:16:38 +0200

    Add sample routing

--- chat-graphql-frontend/src/Pages/Room.js
+++ chat-graphql-frontend/src/Pages/Room.js
@@ -1,14 +1,17 @@
 import React, {Component} from 'react'
+import { Link } from 'react-router-dom';
 
 class Room extends Component {
   sendMessage() {
 
   }
   render() {
-    return <div>I'm in room {this.props.match.params.number} </div>
-    /*
-      render list of messages
-     */
+    return (
+      <div>
+        <div className="roomer">I'm in room {this.props.match.params.number} </div>
+        <Link to="/room/4">123</Link>
+      </div>
+    )
   }
 }
 
--- chat-graphql-frontend/src/Pages/RoomSelector.js
+++ chat-graphql-frontend/src/Pages/RoomSelector.js
@@ -1,5 +1,16 @@
 import React, {Component} from 'react'
 
+/*export default () => {
+  const query = graphql`
+    query AppQuery {
+      allLinks {
+        url
+      }
+    }
+  `
+  return <QueryRenderer environment={environment} query={query} render={App} />
+}*/
+
 class RoomSelector extends Component {
   render() {
     return <div> Here will be selector of room </div>
